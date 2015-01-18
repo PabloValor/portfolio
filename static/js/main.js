2f14a018c8d5440a0d17f433209371b3f723b4bc
Pablo Valor <pablovalor89@gmail.com>
2015-01-18 00:27:39 -0300

    Added hammerjs to nav menu

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,36 @@
 $(function() {
+	var $menuButton = $('#menuButton'),
+		$nav = $('nav'),
+		$body = $('body')[0],
+		mq = window.matchMedia('(max-width: 678px)');
+		hammerBody = new Hammer($body),
+		$arrow = $('nav i');
+
+		//initialize slider
 	$(".rslides").responsiveSlides();
+
+
+	//menu functionality
+
+	function showMenu() {
+		$nav.css('left', '0');
+		$arrow.css('transform','rotate(180deg)');
+	}
+	function hideMenu() {
+		$nav.css('left', '-200px');
+		$arrow.css('transform','rotate(0)');
+	}
+
+	//callback for width viewport changes
+	function widthChange(mq) {
+		if(!mq.matches) {
+			$nav.css('left','0');
+		}
+	}
+
+	hammerBody.on('panright', showMenu);
+	hammerBody.on('panleft', hideMenu);
+
+	mq.addListener(widthChange);
+
 });
